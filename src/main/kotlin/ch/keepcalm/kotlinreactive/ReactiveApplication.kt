11514fcc6457868c66bdef015f7119f35c70d60c
Marcel Widmer <marzelwidmer@gmail.com>
2018-06-02 18:26:32 +0200

    add some sample reactive stuff

--- src/main/kotlin/ch/keepcalm/kotlinreactive/ReactiveApplication.kt
+++ src/main/kotlin/ch/keepcalm/kotlinreactive/ReactiveApplication.kt
@@ -3,18 +3,24 @@ package ch.keepcalm.kotlinreactive
 import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.cloud.gateway.filter.GatewayFilter
-import org.springframework.cloud.gateway.filter.factory.RequestRateLimiterGatewayFilterFactory
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.beans
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
+import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.core.publisher.toMono
 
 @SpringBootApplication
 class ReactiveApplication
@@ -72,5 +78,43 @@ fun main(args: Array<String>) {
             .run(*args)
 }
 
-x
+
 class Info(val profile: String?)
+
+@Component
+class ReactiveHandler(val repo: NamesRepo) {
+    fun getName(search: String): Mono<String> = repo.get(search).toMono().map { "Result: $it!" }
+    fun addName(text: String): Mono<String> = repo.add(text).toMono().map { "Result: $it!" }
+    fun getAllNames(): Flux<String> = repo.getAll().toFlux().map { "Result: $it" }
+}
+
+@Repository
+class NamesRepo {
+    private val entities = mutableListOf<String>()
+    fun add(name: String) = entities.add(name)
+    fun get(name: String) = entities.find { it == name } ?: "not found!"
+    fun getAll() = listOf(entities)
+}
+
+@Configuration
+class RoutingConfiguration {
+
+    @Bean
+    fun routerFunction(handler: ReactiveHandler): RouterFunction<ServerResponse> = router {
+        ("/reactive").nest {
+            val searchPathName = "search"
+            val savePathName = "save"
+            GET("/{$searchPathName}") { req ->
+                val pathVar = req.pathVariable(searchPathName)
+                ok().body(handler.getName(pathVar))
+            }
+            GET("/") {
+                ok().body(handler.getAllNames())
+            }
+            PUT("/{$savePathName}") { req ->
+                val pathVar = req.pathVariable(savePathName)
+                ok().body(handler.addName(pathVar))
+            }
+        }
+    }
+}
