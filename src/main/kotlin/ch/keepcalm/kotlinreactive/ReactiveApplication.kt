cb81736a489604ad956002eeec6405fcfe1b6a8c
Marcel Widmer <marzelwidmer@gmail.com>
2018-06-02 15:45:29 +0200

    Add Router Function

--- src/main/kotlin/ch/keepcalm/kotlinreactive/ReactiveApplication.kt
+++ src/main/kotlin/ch/keepcalm/kotlinreactive/ReactiveApplication.kt
@@ -1,8 +1,14 @@
 package ch.keepcalm.kotlinreactive
 
+import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.support.beans
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlux
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
 
 @SpringBootApplication
 class ReactiveApplication
@@ -12,7 +18,29 @@ fun main(args: Array<String>) {
     SpringApplicationBuilder()
             .sources(ReactiveApplication::class.java)
             .initializers(beans {
+                bean {
+                    WebClient.builder()
+                            .baseUrl("http://127.0.0.1:8080/actuator/info")
+                            .build()
+                }
+                bean {
+                    router {
 
+                        val client = ref<WebClient>()
+                        GET("/profile") {
+                            val profiles: Publisher<String> =
+                                    client
+                                            .get()
+                                            .retrieve()
+                                            .bodyToFlux<Info>()
+                                            .map { it.profile  }
+
+                            ServerResponse.ok().body(profiles)
+                        }
+                    }
+                }
             })
             .run(*args)
 }
+
+class Info (val profile: String?)
